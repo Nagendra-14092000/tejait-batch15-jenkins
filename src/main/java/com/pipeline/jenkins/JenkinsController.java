b24b2871e571b9651652a6ab44ad73e9fb22c4de
Nagendra-14092000 <nagendrababugorrepati@gmail.com>
2026-03-06 11:39:45 +0530

    first commit

---
+++ src/main/java/com/pipeline/jenkins/JenkinsController.java
@@ -0,0 +1,17 @@
+package com.pipeline.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jenkins")
+public class JenkinsController {
+    @GetMapping("welcome")
+    public String welcome(){
+        return "hi jenkins";
+    }@GetMapping("hello")
+    public String hello(){
+        return "hello jenkins";
+    }
+}
